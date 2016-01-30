8ca18c3c9a1a13c7355f258d88341e0259733c74
temirbay <temirbaymiras.ktl@gmai.com>
2016-01-30 14:30:53 +0600

    Prime numbers

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,8 +10,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Press any key to exit.");
+            int n = int.Parse (Console.ReadLine ());
+            int[] array = new int[n + 1];
+            for (int i = 1; i <= n; ++i)
+                array[i] = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; ++i)
+            {
+                bool ok = true;
+                for (int j = 2; j * j <= array[i]; ++j)
+                    if (array[i] % j == 0)
+                        ok = false;
+                if (ok == true)
+                    Console.WriteLine(array[i]);
+            }
             Console.ReadKey();
         }
     }
