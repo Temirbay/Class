edbf1727a5a83dd34e877cfaf368203574b0d048
temirbay <temirbaymiras.ktl@gmai.com>
2016-01-30 19:24:03 +0600

    Student Name Surname gpa

---
+++ Student/Student/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public double gpa;
+
+        public Student (string Name, string Surname)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+        }
+
+        public Student (string Name, string Surname, double gpa)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.gpa = gpa;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Surname + " " +  gpa;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student("Miras", "Temirbay", 4.0);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
