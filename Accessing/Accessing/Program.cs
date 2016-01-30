80666613e56c7c25c91e0fa9b9a79f50e137c11f
temirbay <temirbaymiras.ktl@gmai.com>
2016-01-30 22:59:28 +0600

    Accessing

---
+++ Accessing/Accessing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessing
+{
+    class Program
+    {
+          static void Main(string[] args)
+            {
+                System.Console.WriteLine("Number of command line parameters = {0}", args.Length);
+
+                foreach (string s in args)
+                    System.Console.WriteLine(s);
+
+            Console.ReadKey();
+            }
+        }
+    
+}
