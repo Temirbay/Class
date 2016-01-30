150250694780e087d50cbedca2c891e4cd88a14d
temirbay <temirbaymiras.ktl@gmai.com>
2016-01-30 19:30:15 +0600

    TestClass

---
+++ TestClass/TestClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClass
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(args.Length);
+            Console.ReadKey();
+        }
+    }
+    
+}
