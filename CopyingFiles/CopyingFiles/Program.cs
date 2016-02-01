4b242a516169a8f60e198e9e563ad9395da14e59
temirbay <temirbaymiras.ktl@gmai.com>
2016-02-01 23:30:09 +0600

    Creating File or Folder

---
+++ CopyingFiles/CopyingFiles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyingFiles
+{
+    public class SimpleFileCopy
+    {
+        static void Main()
+        {
+            string fileName = "test.txt";
+            string From = @"C:\Users\Public\TestFolder";
+            string To = @"C:\Users\Public\TestFolder\SubFolder";
+
+            string sourceFile = Path.Combine(From, fileName);
+            string destFile = Path.Combine(To, fileName);
+
+            if (!Directory.Exists(To))
+            {
+                Directory.CreateDirectory(To);
+            }
+
+            File.Copy(sourceFile, destFile, true);
+
+            if (Directory.Exists(From))
+            {
+                string[] files = Directory.GetFiles(From);
+
+                foreach (string s in files)
+                {
+                    fileName = Path.GetFileName(s);
+                    destFile = Path.Combine(To, fileName);
+                    File.Copy(s, destFile, true);
+                }
+            }
+            else
+               Console.WriteLine("Source path does not exist!");
+            
+            Console.ReadKey();
+        }
+    }
+}
