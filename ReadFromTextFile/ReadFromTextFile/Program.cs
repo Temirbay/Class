e93a1e6f2ab97b36ddcca51f2ea75f231f790a83
temirbay <temirbaymiras.ktl@gmai.com>
2016-02-01 23:42:02 +0600

    Reading from text file

---
+++ ReadFromTextFile/ReadFromTextFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromTextFile
+{
+    class ReadFromFile
+    {
+        static void Main()
+        {
+            string text = File.ReadAllText(@"C:\Users\Public\TestFolder\WriteText.txt");
+            Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            string[] lines = File.ReadAllLines(@"C:\Users\Public\TestFolder\Name.txt");
+            
+            Console.WriteLine("Contents of Name.txt = ");
+            foreach (string line in lines)
+                Console.WriteLine("\t" + line);
+            
+            System.Console.ReadKey();
+        }
+    }
+}
