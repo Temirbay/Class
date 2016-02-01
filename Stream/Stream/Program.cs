4020de55ebc8b25e2cb397d90ede103b3f049448
temirbay <temirbaymiras.ktl@gmai.com>
2016-02-02 00:08:27 +0600

    Stream

--- Stream/Stream/Program.cs
+++ Stream/Stream/Program.cs
@@ -7,39 +7,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication3
 {
-    // f("testfolder") = count_of_files + f("sdf") + f("test") + f("test1)
-    // f("test1") = count_of_files + f("aaa") 
-
     class Program
     {
         static void Main(string[] args)
         {
-            F3();
+            Function();
             Console.ReadKey();
         }
 
-        static void F3()
+        static void Function()
         {
-            FileStream fread = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            FileStream fwrite = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream fread = new FileStream(@"C:\Users\Public\TestFolder\input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream fwrite = new FileStream(@"C:\Users\Public\TestFolder\output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
 
             StreamReader sr = new StreamReader(fread);
             StreamWriter sw = new StreamWriter(fwrite);
 
-            string[] token = sr.ReadLine().Split();
-            // token[0] = 1
-            // token[1] = 23 ...
             int sum = 0;
             while (sr.Peek() >= 0)
             {
                 string s = sr.ReadLine();
-                int num = new;
+                int num = int.Parse(s);
+                sum += num;
+            }
 
-            }
-            for (int i = 0; i < token.Length; i++)
-            {
-                sum += int.Parse(token[i]);
-            }
             sw.WriteLine(sum);
 
             sw.Close();
@@ -47,6 +38,5 @@ namespace ConsoleApplication3
             fwrite.Close();
             fread.Close();
         }
-        
     }
 }
