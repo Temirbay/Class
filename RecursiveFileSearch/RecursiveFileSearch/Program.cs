01d35cc254b2fdf73ae9cf50cc3e5ad4c09ce6ec
temirbay <temirbaymiras.ktl@gmai.com>
2016-02-01 17:31:10 +0600

    Recursive File Searching

--- RecursiveFileSearch/RecursiveFileSearch/Program.cs
+++ RecursiveFileSearch/RecursiveFileSearch/Program.cs
@@ -9,31 +9,25 @@ namespace Example_FileInfo
 {
     class Program
     {
-        public static void Walk (DirectoryInfo d, int depth)
+        public static void Walk(DirectoryInfo d, int depth)
         {
             FileInfo[] F = d.GetFiles();
             foreach (FileInfo i in F)
             {
                 for (int ii = 0; ii < depth; ++ii) Console.Write("  ");
                 Console.WriteLine("File Name {0}", i.Name);
-                Console.ReadKey();
             }
 
             DirectoryInfo[] D = d.GetDirectories();
             foreach (DirectoryInfo i in D)
-            {
                 Walk(i, depth + 1);
-            }
         }
 
         static void Main(string[] args)
         {
             DirectoryInfo d = new DirectoryInfo(@"D:\films");
-
             Console.WriteLine("Directory name: films");
-
             Walk(d, 0);
-
             Console.ReadKey();
         }
     }
