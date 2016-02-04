7cb04392a648a028fb0c499e5e18442ed4a980ca
temirbay <temirbaymiras.ktl@gmai.com>
2016-02-04 22:15:56 +0600

    Max Min form given numbers

---
+++ MaxFromStream/MaxFromStream/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxFromStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fread = new FileStream(@"C:\PT_labs\TestFolder\input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream fwrite = new FileStream(@"C:\PT_labs\TestFolder\output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            StreamReader fr = new StreamReader(fread);
+            StreamWriter fw = new StreamWriter(fwrite);
+
+            int MAX = -1000000000;
+            int MIN = 1000000000;
+
+            while (fr.Peek() >= 0)
+            {
+                string s = fr.ReadLine();
+                int num = int.Parse(s);
+
+                if (num < MIN) MIN = num;
+                if (num > MAX) MAX = num; 
+            }
+
+            fw.WriteLine(MAX);
+            fw.WriteLine(MIN);
+
+            fr.Close();
+            fw.Close();
+            fread.Close();
+            fwrite.Close();
+        }
+    }
+}
---
+++ Snake_Forms/Snake_Forms/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_Forms
+{
+    public partial class Form1 : Form
+    {
+        static void SnakeDrawer(int x, int y)
+        {
+
+        }
+
+        static Key
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            SnakeDrawer(1, 1);
+        }
+    }
+}
