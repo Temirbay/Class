7526f8a80e2b21936d974c6df7a70fb9fbfa56ab
temirbay <temirbaymiras.ktl@gmai.com>
2016-04-02 12:44:39 +0600

    graphics

---
+++ Change_Timer/Change_Timer/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_Timer
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        SolidBrush brush;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+            brush = new SolidBrush(Color.Red);
+            timer1.Start();
+            timer1.Interval = 200;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            brush.Color = brush.Color == Color.Red ? Color.Blue : Color.Red;
+            g.FillRectangle(brush, 0, 0, Width, Height);
+
+        }
+    }
+}
